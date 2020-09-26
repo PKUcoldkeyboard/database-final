2ed61749c07f151d7c812e30967a2dc5231f1d29
Pang Shangzhi <158505862@qq.com>
2020-09-26 14:10:10 +0800

    创建spring boot项目和vue项目

---
+++ db-final/src/main/java/com/cuterwrite/dbfinal/DbFinalApplication.java
@@ -0,0 +1,13 @@
+package com.cuterwrite.dbfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbFinalApplication.class, args);
+	}
+
+}
