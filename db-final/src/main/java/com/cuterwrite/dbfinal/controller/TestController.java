b20ca5ef5b955717f5ccb30c414637fb4ea4095f
Pang Shangzhi <158505862@qq.com>
2020-11-12 11:33:32 +0800

    add PreoAuthorize

--- db-final/src/main/java/com/cuterwrite/dbfinal/controller/TestController.java
+++ db-final/src/main/java/com/cuterwrite/dbfinal/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.cuterwrite.dbfinal.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 	@GetMapping("/test")
 	public String test() {
 		return "Hello Cuterwrite Jenkins";
