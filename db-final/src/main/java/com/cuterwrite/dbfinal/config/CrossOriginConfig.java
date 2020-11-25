4a520f1f3bbc5fe2321cd3a6342e09b790a44c60
Pang Shangzhi <158505862@qq.com>
2020-11-25 14:49:26 +0800

    update corsConfig

--- db-final/src/main/java/com/cuterwrite/dbfinal/config/CrossOriginConfig.java
+++ db-final/src/main/java/com/cuterwrite/dbfinal/config/CrossOriginConfig.java
@@ -24,9 +24,9 @@ public class CrossOriginConfig {
 				//添加映射路径
 				registry.addMapping("/**")
 				.allowedOrigins(allowedOrigins) //请求地址
-				.allowCredentials(true) //发送Cookie
+				.allowedHeaders("*")
 				.allowedMethods("*") //请求方式
-				.allowedHeaders("*");
+				.allowCredentials(true) ;//发送Cookie; 
 			}
 		};
 	}
