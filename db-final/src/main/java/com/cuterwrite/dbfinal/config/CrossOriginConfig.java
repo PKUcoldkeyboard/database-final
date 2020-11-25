7477953941efa73de887435278774a24b98bcccf
Pang Shangzhi <158505862@qq.com>
2020-11-25 15:21:40 +0800

    fixed

--- db-final/src/main/java/com/cuterwrite/dbfinal/config/CrossOriginConfig.java
+++ db-final/src/main/java/com/cuterwrite/dbfinal/config/CrossOriginConfig.java
@@ -24,7 +24,7 @@ public class CrossOriginConfig {
 				//添加映射路径
 				registry.addMapping("/**")
 				.allowedOrigins(allowedOrigins) //请求地址
-				.allowedHeaders("Content-Type","Access-Token","Appid","Secret","Authorization")
+				.allowedHeaders("*")
 				.allowedMethods("*") //请求方式
 				.allowCredentials(true) ;//发送Cookie; 
 			}
