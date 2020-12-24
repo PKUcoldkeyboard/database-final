1823e2da34b8793fc36decab3421d95df0e35310
Pang Shangzhi <158505862@qq.com>
2020-12-25 00:08:40 +0800

    修改elasticsearch、kibana、fluentd的服务地址

--- db-final/src/main/java/com/cuterwrite/dbfinal/elasticsearch/dao/BookRepository.java
+++ db-final/src/main/java/com/cuterwrite/dbfinal/elasticsearch/dao/BookRepository.java
@@ -1,15 +1,16 @@
 package com.cuterwrite.dbfinal.elasticsearch.dao;
 
 
+import java.util.List;
+
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import com.cuterwrite.dbfinal.entity.User;
+import com.cuterwrite.dbfinal.entity.Book;
 
 /**  
  * @author Pang S.Z.
  * @create 2020-10-12 14:33:31 
  */
-public interface BookRepository extends ElasticsearchRepository<User, Long>{
-	
-
+public interface BookRepository extends ElasticsearchRepository<Book, Long>{
+	List<Book>findByName(String name);
 }
--- db-final/src/test/java/com/cuterwrite/dbfinal/DbFinalApplicationTests.java
+++ db-final/src/test/java/com/cuterwrite/dbfinal/DbFinalApplicationTests.java
@@ -3,10 +3,16 @@ package com.cuterwrite.dbfinal;
 
 
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.cuterwrite.dbfinal.elasticsearch.dao.BookRepository;
+import com.cuterwrite.dbfinal.entity.Book;
 import com.cuterwrite.dbfinal.service.TestService;
 
 
@@ -17,10 +23,20 @@ import com.cuterwrite.dbfinal.service.TestService;
 @SpringBootTest
 class DbFinalApplicationTests {
 	@Autowired
-	TestService service;
+	BookRepository repository;
 	@Test
 	void contextLoads() {
-		service.insertUserInfo();
+		Book book=new Book();
+		/*
+		 * book.setAlbumPicture("测试") .setAuthor("测试") .setCategoryId(1L)
+		 * .setCreateTime(new Date()) .setModifyTime(new Date()) .setDescription("测试")
+		 * .setId(1L) .setIsbn("测试") .setLanguage("测试") .setName("测试") .setPageCount(1)
+		 * .setPicture("测试") .setPress("测试") .setPrice(new BigDecimal("99.9"))
+		 * .setPublishDate(new Date()) .setSale(1) .setStock(1);
+		 */
+		List<Book>books=repository.findByName("adsad");
+		Book b=books.get(0);
+		System.out.println(b.toString());
 	}
 
 }
