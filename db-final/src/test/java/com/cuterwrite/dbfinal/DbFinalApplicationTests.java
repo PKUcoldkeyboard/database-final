af1833cbe526de70558e3d2601bdeaba556cdc8f
Pang Shangzhi <158505862@qq.com>
2021-03-03 19:54:37 +0800

    end

--- db-final/src/test/java/com/cuterwrite/dbfinal/DbFinalApplicationTests.java
+++ db-final/src/test/java/com/cuterwrite/dbfinal/DbFinalApplicationTests.java
@@ -8,13 +8,8 @@ package com.cuterwrite.dbfinal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 
-import com.cuterwrite.dbfinal.common.ResponseResult;
-import com.cuterwrite.dbfinal.entity.EsBook;
 import com.cuterwrite.dbfinal.service.BookSearchService;
-
-import cn.hutool.core.bean.BeanUtil;
 
 
 
